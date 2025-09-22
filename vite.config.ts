76d113ea43876cca449279777f9567d7881e6604
unknown <fariasvalentino25@gmail.com>
2025-09-22 19:35:02 -0300

    readyt for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+      },
+    },
+  },
+
   server: {
     host: true,
     port: 5173,
     strictPort: true,
-    watch: {
-      usePolling: true
-    },
-    allowedHosts: ["fuzzy-socks-retire.loca.lt"],
+    watch: { usePolling: true },
     proxy: {
       "/api": {
-        target: "http://localhost:8080", 
+        target: "http://localhost:8080",
         changeOrigin: true,
       },
     },
   },
-})
+
+  base: '/', 
+});
